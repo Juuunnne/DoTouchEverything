d621a87aa5573c7c756f24e432ca5c03a52fb8bc
Jun <jzhi@gaming.tech>
2024-03-05 21:45:37 +0100

    Add TextBox

---
+++ Assets/_Project/Scripts/Utils/TextBox.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBox : MonoBehaviour
+{
+    [SerializeField] private Image _background;
+    [SerializeField] private TextMeshProUGUI _text;
+    void Update()
+    {
+        if (_text.text == "")
+        {
+            _background.enabled = false;
+        }
+        else
+        {
+            _background.enabled = true;
+            _background.rectTransform.sizeDelta = new Vector2(_text.preferredWidth + 20, _text.preferredHeight + 20);
+        }
+    }
+    
+    public void SetText(string text)
+    {
+        _text.text = text;
+    }
+}
