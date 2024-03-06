2dcca04f3f37318e3cb7b2166f744edc22d06094
Lucas Vocanson <lucas.vocanson@gmail.com>
2024-03-06 17:24:33 +0100

    Display

---
+++ Assets/_Project/Scripts/Mini Games/Bottle Mastermind/BottleMastermindDisplay.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class BottleMastermindDisplay : MonoBehaviour
+{
+    [SerializeField]
+    private BottleMastermindGame _game = null;
+
+    [SerializeField]
+    private UnityEngine.UI.Text _text = null;
+
+    private void Start()
+    {
+        _game.OnGameWon.AddListener(OnGameWon);
+        _game.OnGuessWrong.AddListener(OnGuessWrong);
+    }
+
+    private void OnGameWon()
+    {
+        _text.text = "You won!";
+    }
+
+    private void OnGuessWrong(int correctDigits)
+    {
+        _text.text = "Score:\n" + correctDigits;
+    }
+
+    private void OnDestroy()
+    {
+        _game.OnGameWon.RemoveListener(OnGameWon);
+        _game.OnGuessWrong.RemoveListener(OnGuessWrong);
+    }
+}
