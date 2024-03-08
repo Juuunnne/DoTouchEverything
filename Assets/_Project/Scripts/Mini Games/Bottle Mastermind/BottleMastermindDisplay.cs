3ca13edf848e9ae6a4a568b6ea340f5c2310baf9
Lucas Vocanson <lucas.vocanson@gmail.com>
2024-03-08 11:38:12 +0100

    Fix BottleMasterMind

--- Assets/_Project/Scripts/Mini Games/Bottle Mastermind/BottleMastermindDisplay.cs
+++ Assets/_Project/Scripts/Mini Games/Bottle Mastermind/BottleMastermindDisplay.cs
@@ -4,30 +4,15 @@ using UnityEngine;
 public class BottleMastermindDisplay : MonoBehaviour
 {
     [SerializeField]
-    private BottleMastermindGame _game = null;
-
-    [SerializeField]
     private TextMeshPro _text = null;
 
-    private void Start()
-    {
-        _game.OnGameWon.AddListener(OnGameWon);
-        _game.OnGuessWrong.AddListener(OnGuessWrong);
-    }
-
-    private void OnGameWon()
+    public void OnGameWon()
     {
         _text.text = "You won!";
     }
 
-    private void OnGuessWrong(int correctDigits)
+    public void OnGuessWrong(int correctDigits)
     {
         _text.text = "Score:\n" + correctDigits;
-    }
-
-    private void OnDestroy()
-    {
-        _game.OnGameWon.RemoveListener(OnGameWon);
-        _game.OnGuessWrong.RemoveListener(OnGuessWrong);
     }
 }
