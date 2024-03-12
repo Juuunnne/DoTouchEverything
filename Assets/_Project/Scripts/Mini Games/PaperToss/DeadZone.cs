e41a949100ebfdd089b5cb649c6911ce1873c9ed
Gotvna <nvogt@gaming.tech>
2024-03-12 12:07:34 +0100

    Add DeadZone and Respawn Zone

---
+++ Assets/_Project/Scripts/Mini Games/PaperToss/DeadZone.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class DeadZone : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider _respawnZone;
+
+    [Header("Variables")]
+    [SerializeField]
+    private float _cooldownTime = 5f;
+
+    private BoxCollider _deadZone;
+
+    private void Start()
+    {
+        _deadZone = GetComponent<BoxCollider>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("ScoreObject"))
+        {
+            Debug.Log("Object entered dead zone");
+            StartCoroutine(ReplaceObject(collision.gameObject));
+            Debug.Log("Coroutine Start");
+        }
+    }
+
+    private IEnumerator ReplaceObject(GameObject objToReplace)
+    {
+        yield return new WaitForSeconds(_cooldownTime);
+        if (_deadZone.bounds.Contains(objToReplace.transform.position))
+        {
+            yield break;
+        }
+
+        objToReplace.transform.position = new Vector3(
+            Random.Range(_respawnZone.bounds.min.x, _respawnZone.bounds.max.x),
+            Random.Range(_respawnZone.bounds.min.y, _respawnZone.bounds.max.y),
+            Random.Range(_respawnZone.bounds.min.z, _respawnZone.bounds.max.z));
+    }
+}
