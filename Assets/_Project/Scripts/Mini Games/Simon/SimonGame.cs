81afd30d169809618f0fff57e90dd564c8ccb6d2
Gotvna <nvogt@gaming.tech>
2024-03-13 09:51:09 +0100

    remove debug.log and add modification to avoid wrong button

--- Assets/_Project/Scripts/Mini Games/Simon/SimonGame.cs
+++ Assets/_Project/Scripts/Mini Games/Simon/SimonGame.cs
@@ -42,7 +42,6 @@ public class SimonGame : MiniGame
 
     private void OnButtonPressed(ButtonType buttonType)
     {
-        Debug.Log("Button pressed: " + buttonType);
         if (!_canPlay)
         {
             return;
@@ -57,7 +56,6 @@ public class SimonGame : MiniGame
             else
             {
                 _textBox.text = "Wrong button!";
-                Debug.Log($"Button type:{buttonType}" + $"{_sequence[_currentSequenceIndex]}");
             }
         }
         if (_currentSequenceIndex >=  _sequenceIndex)
@@ -69,7 +67,6 @@ public class SimonGame : MiniGame
 
         if (_sequence.Length == 0)
         {
-            Debug.Log("Sequence completed");
             OnGameWon.Invoke();
         }
     }
@@ -93,7 +90,6 @@ public class SimonGame : MiniGame
         _textBox.text = "Wait for the next sequence!";
         for (int i = 0; i < currentIndex; i++)
         {
-            Debug.Log("Button: " + _sequence[i]);
             _simonButtons[(int)_sequence[i]].HighlightButton(true);
             yield return new WaitForSeconds(SequenceDelay);
             _simonButtons[(int)_sequence[i]].HighlightButton(false);
@@ -103,9 +99,9 @@ public class SimonGame : MiniGame
 
     private void NextSequence()
     {
+        _currentSequenceIndex = 0;
         if (_sequence.Length == 0)
         {
-            Debug.Log("Sequence completed");
             OnGameWon.Invoke();
         }
         _textBox.text = $"{_sequenceIndex}/{SequenceLength}";
