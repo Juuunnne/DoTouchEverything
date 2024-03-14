7cae8fb6e3ce929563726f40afcb52fb5976f08a
Gotvna <nvogt@gaming.tech>
2024-03-14 14:30:34 +0100

    Fix Read/Write acces on fbx

--- Assets/_Project/Scripts/Mini Games/PaperToss/BinCollisionHandler.cs
+++ Assets/_Project/Scripts/Mini Games/PaperToss/BinCollisionHandler.cs
@@ -3,18 +3,23 @@ using UnityEngine;
 public class ScoreZoneCollisionHandler : MonoBehaviour
 {
     [SerializeField] private ParticleSystem Confetti;
-
     [SerializeField] private PaperTossGame _paperTossGame;
-    private AudioSource _audioSource;
+    [SerializeField] private AudioClip _audioClip;
+    [SerializeField] private AudioSource _audioSource;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("ScoreObject"))
         {
+            Confetti.Play();
+            _audioSource.PlayOneShot(_audioClip);
             _paperTossGame.IncrementScore();
             Destroy(other.gameObject);
-            Confetti.Play();
-            _audioSource.Play();
         }
     }
 }
